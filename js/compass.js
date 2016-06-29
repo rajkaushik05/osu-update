529f259983dfab08d0fc2b25c16f8c3370355653
rajkaushik05 <rajbir.kaushik@srijan.in>
2016-06-29 18:19:02 +0530

    compass-3 slider page next and prev functionality update

--- js/compass.js
+++ js/compass.js
@@ -241,21 +241,49 @@
 			var els = '<li class="'+anchorName.name+' '+isActive+'"><input  ' +isChecked +' data-parent="'+index+'" type="checkbox" id="'+elId+'" ><label for="'+elId+'"><span class="icon"></span><span class="count">' + count + '</span>'+ anchorName.evidence[item] +'</label></li>';               
 			$('#anchorDetails').append(els);
 		}
-    }
+    };
+
+    var arrowOffset = function(index){
+		var x = index.offset().left;
+		x += 28;
+		newPos = new Object();
+        newPos.left = x;
+        newPos.top = "160px";
+        $('.anchor-arrow').offset(newPos);
+	};
+
+
 
     var clickedItem = [];
+    var selectedItemIndex = [];
+
     $('#anchorSlider li').click(function(){ 
    		showBehaviorInBlock(anchorCollection[$(this).index()], $(this).index());
-
    		var index = ($(this).index());
    		var prev = Store[index] || [];
-   		clickedItem.push(index);
+   		arrowOffset($(this));
+   		if(clickedItem.indexOf(index) == -1){
+			clickedItem.push(index);	
+		}
    		
-   		var selectedItemIndex = [];
-   		for(el in Store){
+   		/*for(el in Store){
    			selectedItemIndex.push(el);
-   		}
+   		}*/
 
+   		/* Back to brev anchor button on bottom*/
+   		if(index > 0){
+			$('#backToAnchor').show();
+		} else if(index == 0){
+			$('#backToAnchor').hide();
+		}
+		var preAnchorText = $(this).prev().find('.name').text();
+		$('#backToAnchor .update').text(preAnchorText);
+
+		/*Let's Ranking botton link type button */
+		var size  = Object.keys(Store).length;
+		if(size > 0){
+			$('.lets-ranking, .lets-ranking .lets-ranking-btn').show();
+		}
 
    		for(el in clickedItem){
    			var indexValue = clickedItem[el];
@@ -268,9 +296,14 @@
 
    		var lastIndex = index+1;
    		if(lastIndex == anchorCollection.length) {
-   			$('.action p').hide();
+   			$('.action #nextAnchorBlock').hide();
+   			$('#nextAnchorBtn').hide();
+			$('#letsRankingBTN').show();
+			$('.lets-ranking').hide();
    		} else {
-   			$('.action p').show();
+   			$('.action #nextAnchorBlock').show();
+   			$('#nextAnchorBtn').show();
+			$('#letsRankingBTN').hide();
    		}
    		var anchorText = $(this).next().find('.name').text();
    		var currentAnchorText = $(this).find('.name').text();
@@ -281,11 +314,11 @@
    		$(this).removeClass().addClass('active');
    		$('#thirdStepCompassBtn').show();
    		$('#thirdStepCompassContinueBtn').hide();
-   		$('#anchorSlider .anchor-arrow').show();
-   		$('.action .btn-back').show();   		
+   		$('#anchorSlider .anchor-arrow').show();	
    		$('.anchor-detail-section .anchor-des').show();
    		$('.anchor-detail-section .ranking-block-text').hide();
     });
+
 
     $('#anchorDetails').on("change", "input[type='checkbox']",  function(){
     	var index =  $(this).attr('data-parent');
@@ -306,26 +339,26 @@
 	
 $(document).ready(function(){
 
+	
 	/* Add PERSONALITY block compass-1.html page*/
 	
 	/* code for compass page */
 
 	$('.anchor-list li').click(function(){
 		var elId = $(this).attr('id');
-		//var temp = 'compass-3.html#' + elId;
 		window.location.href = 'compass-3.html?=' + elId;
 	});
 
 	/*Active slider item arrow position get*/
 
-	$('.bxslider li').click(function(){
+	/*$('.bxslider li').click(function(){
 		var x = $(this).offset().left;
 		x += 28;
 		newPos = new Object();
         newPos.left = x;
         newPos.top = "160px";
         $('.anchor-arrow').offset(newPos);
-	});
+	});*/
 
 	/* Compass third step continue button */
 	$('#thirdStepCompassContinueBtn').click(function(){
@@ -334,7 +367,7 @@ $(document).ready(function(){
 	});
 
 	/* Start final selected Anchor Evidence list and Drag and Drop functionality*/
-	$('#thirdStepCompassBtn').click(function(){
+	$('.lets-ranking-btn').click(function(){
 		$('#anchorDetails').empty();
 		//var size  = Object.keys(Store).length;
         var prev = [];
@@ -377,6 +410,7 @@ $(document).ready(function(){
         $('#thirdStepCompassContinueBtn').show();
         $('#anchorSlider .anchor-arrow').hide();
         $('.action .btn-back, .action p').hide();
+        $('#nextAnchorBtn').hide();
         
 		return false
 	});
@@ -419,7 +453,126 @@ $(document).ready(function(){
    			$('#nextAnchorText').text(anchorText);
    			var currentAnchorText = $(this).find('.name').text();
    			$('#currentAnchorText').text(currentAnchorText);
+
+   			if(i > 0){
+   				$('#backToAnchor').show();
+   			}
 		}
+	});
+
+	/* bottom button NEXT click next anchor */
+	$('#nextAnchorBtn').click(function(){
+		var itemClass;
+		$('#anchorSlider li').each(function(i, elem){
+		  if($(elem).hasClass('active')){
+		    itemClass = i;
+		    return false
+		  }
+		});
+
+		var size  = Object.keys(Store).length;
+		if(size > 0){
+		 	$('.lets-ranking, .lets-ranking .lets-ranking-btn').show();
+		}
+
+		var nextAnchorIndex = itemClass+1;
+		if(nextAnchorIndex+1 === anchorCollection.length){
+			$('.lets-ranking, .lets-ranking .lets-ranking-btn').hide();
+			$('#nextAnchorBtn').hide();
+         	$('#letsRankingBTN').show();
+         	$('#nextAnchorBlock').hide();
+		} else if(nextAnchorIndex >= 1){
+			$('#backToAnchor').show();
+			$('#nextAnchorBtn').show();
+			$('#nextAnchorBlock').show();
+		}
+
+		var preAnchorText = $('#anchorSlider li').eq(itemClass).find('.name').text();
+      	$('#backToAnchor .update').text(preAnchorText);
+      	var nextAnchorIndex2 = nextAnchorIndex+1;
+      	var nextAnchorText = $('#anchorSlider li').eq(nextAnchorIndex2).find('.name').text();
+      	$('#nextAnchorText').text(nextAnchorText);
+		var currentAnchorText = $('#anchorSlider li').eq(nextAnchorIndex).find('.name').text();
+		$('#currentAnchorText').text(currentAnchorText);
+		
+
+		$('#anchorSlider li').removeClass('active');
+		var nextAnchor = $('#anchorSlider li').eq(nextAnchorIndex);
+		arrowOffset(nextAnchor);
+		showBehaviorInBlock(anchorCollection[nextAnchorIndex], nextAnchorIndex);
+
+		if(clickedItem.indexOf(nextAnchorIndex) == -1){
+	        clickedItem.push(itemClass);   
+	    }
+
+        for(el in clickedItem){
+            var indexValue = clickedItem[el];
+            if(Store[indexValue] == undefined){
+               $('#anchorSlider li').eq(indexValue).removeClass().addClass('ignore');  
+            } else if(Store[indexValue] !== undefined){
+               $('#anchorSlider li').eq(indexValue).removeClass().addClass('done');
+            }
+         }
+
+         $('#anchorSlider li').eq(nextAnchorIndex).removeClass().addClass('active');
+
+
+		return false
+	});
+
+	/* bottom button PREVIOUS ANCHOR click  */
+	$('#backToAnchor').click(function(){
+		var itemClass;
+		$('#anchorSlider li').each(function(i, elem){
+		  if($(elem).hasClass('active')){
+		    itemClass = i;
+		    return false
+		  }
+		});
+
+		var size  = Object.keys(Store).length;
+		if(size > 0){
+		 	$('.lets-ranking, .lets-ranking .lets-ranking-btn').show();
+		}
+
+		if(itemClass == 1){
+			$('#backToAnchor').hide();
+		} 
+		var nextAnchorIndex = itemClass-1;
+		var preAnchorIndex2 = nextAnchorIndex-1;
+		var preAnchorText = $('#anchorSlider li').eq(preAnchorIndex2).find('.name').text();
+      	$('#backToAnchor .update').text(preAnchorText);
+      	var nextAnchorText = $('#anchorSlider li').eq(itemClass).find('.name').text();
+      	$('#nextAnchorText').text(nextAnchorText);
+		var currentAnchorText = $('#anchorSlider li').eq(nextAnchorIndex).find('.name').text();
+		$('#currentAnchorText').text(currentAnchorText);
+		
+
+		$('#anchorSlider li').removeClass('active');
+		var nextAnchor = $('#anchorSlider li').eq(nextAnchorIndex);
+		arrowOffset(nextAnchor);
+		showBehaviorInBlock(anchorCollection[nextAnchorIndex], nextAnchorIndex);
+
+		if(clickedItem.indexOf(itemClass) == -1){
+	        clickedItem.push(itemClass);   
+	    }
+
+        for(el in clickedItem){
+            var indexValue = clickedItem[el];
+            if(Store[indexValue] == undefined){
+               $('#anchorSlider li').eq(indexValue).removeClass().addClass('ignore');  
+            } else if(Store[indexValue] !== undefined){
+               $('#anchorSlider li').eq(indexValue).removeClass().addClass('done');
+            }
+         }
+
+         $('#letsRankingBTN').hide();
+         $('#nextAnchorBtn, #nextAnchorBlock').show();
+         
+         $('#anchorSlider li').eq(nextAnchorIndex).removeClass().addClass('active');
+
+
+		return false
 	});
 
 
